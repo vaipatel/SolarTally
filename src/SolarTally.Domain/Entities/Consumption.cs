12561d017a3b2dbee21adea537f42601cd0e14c0
vaipatel <vai.pateln@gmail.com>
2019-10-19 06:23:42 -0400

    Add List of ApplianceUsages.
    I'm keeping this readonly cuz that's what I'm seeing in the examples.

--- src/SolarTally.Domain/Entities/Consumption.cs
+++ src/SolarTally.Domain/Entities/Consumption.cs
@@ -1,9 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
 using SolarTally.Domain.Common;
+using SolarTally.Domain.Interfaces;
+using Ardalis.GuardClauses;
 
 namespace SolarTally.Domain.Entities
 {
-    public class Consumption : BaseEntity<int>
+    public class Consumption : BaseEntity<int>, IAggregateRoot
     {
-        
+        private readonly List<ApplianceUsage> _applianceUsages;
+        public IReadOnlyCollection<ApplianceUsage> ApplianceUsages =>
+            _applianceUsages;
+
+        public Consumption()
+        {
+            _applianceUsages = new List<ApplianceUsage>();
+        }
     }
 }
