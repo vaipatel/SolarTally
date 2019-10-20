bec5705e5d75ffbe3b2ff624509a9213bcaed307
vaipatel <vai.pateln@gmail.com>
2019-10-20 05:24:14 -0400

    Add some tests for ApplianceUsage ctor
    Mostly checking guards here

---
+++ tests/SolarTally.UnitTests/Domain/Entities/ApplianceUsageCtor.cs
@@ -0,0 +1,87 @@
+using System;
+using Xunit;
+using SolarTally.Domain.Entities;
+using SolarTally.UnitTests.Builders;
+
+namespace SolarTally.UnitTests.Domain.Entities
+{
+    public class ApplianceUsageCtor
+    {
+        [Fact]
+        void ThrowsForNullAppliance()
+        {
+            var builder = new ApplianceUsageBuilder();
+            Assert.Throws<ArgumentNullException>(() => {
+                new ApplianceUsage(null, builder.TestQuantity,
+                    builder.TestPowerConsumption, builder.TestNumHours,
+                    builder.TestPercentHrsOnSolar, builder.TestEnabled);
+            });
+        }
+
+        [Fact]
+        void ThrowsForNegativeQuantity()
+        {
+            var builder = new ApplianceUsageBuilder();
+            Assert.Throws<ArgumentOutOfRangeException>(() => {
+                new ApplianceUsage(builder.TestAppliance, -1,
+                    builder.TestPowerConsumption, builder.TestNumHours,
+                    builder.TestPercentHrsOnSolar, builder.TestEnabled);
+            });
+        }
+
+        [Fact]
+        void ThrowsForNegativePowerConsumption()
+        {
+            var builder = new ApplianceUsageBuilder();
+            Assert.Throws<ArgumentOutOfRangeException>(() => {
+                new ApplianceUsage(builder.TestAppliance, builder.TestQuantity,
+                    -10.5m, builder.TestNumHours, builder.TestPercentHrsOnSolar,
+                    builder.TestEnabled);
+            });
+        }
+
+        [Fact]
+        void ThrowsForNegativeNumHours()
+        {
+            var builder = new ApplianceUsageBuilder();
+            Assert.Throws<ArgumentOutOfRangeException>(() => {
+                new ApplianceUsage(builder.TestAppliance, builder.TestQuantity,
+                    builder.TestPowerConsumption, -1,
+                    builder.TestPercentHrsOnSolar, builder.TestEnabled);
+            });
+        }
+
+        [Fact]
+        void ThrowsForGreaterThan24NumHours()
+        {
+            var builder = new ApplianceUsageBuilder();
+            Assert.Throws<ArgumentOutOfRangeException>(() => {
+                new ApplianceUsage(builder.TestAppliance, builder.TestQuantity,
+                    builder.TestPowerConsumption, 25,
+                    builder.TestPercentHrsOnSolar, builder.TestEnabled);
+            });
+        }
+
+        [Fact]
+        void ThrowsForNegativePercentHrsOnSolar()
+        {
+            var builder = new ApplianceUsageBuilder();
+            Assert.Throws<ArgumentOutOfRangeException>(() => {
+                new ApplianceUsage(builder.TestAppliance, builder.TestQuantity,
+                    builder.TestPowerConsumption, builder.TestNumHours,
+                    -1.1m, builder.TestEnabled);
+            });
+        }
+
+        [Fact]
+        void ThrowsForGreaterThan1PercentHrsOnSolar()
+        {
+            var builder = new ApplianceUsageBuilder();
+            Assert.Throws<ArgumentOutOfRangeException>(() => {
+                new ApplianceUsage(builder.TestAppliance, builder.TestQuantity,
+                    builder.TestPowerConsumption, builder.TestNumHours,
+                    -1, builder.TestEnabled);
+            });
+        }
+    }
+}
