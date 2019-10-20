b3d7a47d7d18c5c39826a42d14e569b2d01e02cd
vaipatel <vai.pateln@gmail.com>
2019-10-20 01:21:18 -0400

    Use ApplianceBuilder in ApplianceCtor tests

--- tests/SolarTally.UnitTests/Domain/Entities/ApplianceCtor.cs
+++ tests/SolarTally.UnitTests/Domain/Entities/ApplianceCtor.cs
@@ -1,26 +1,27 @@
 using System;
 using Xunit;
 using SolarTally.Domain.Entities;
+using SolarTally.UnitTests.Builders;
 
 namespace SolarTally.UnitTests.Domain.Entities
 {
     public class ApplianceCtor
     {
-        private string _applianceName = "AN_APPLIANCE";
-        private string _applianceDesc = "AN_APPLIANCE_DESC";
-
         [Fact]
         void ThrowsForNegativeDefaultPowerConsumption()
         {
+            var builder = new ApplianceBuilder();
             Assert.Throws<ArgumentOutOfRangeException>(() => {
-                new Appliance(_applianceName, _applianceDesc, -10.5m);
+                new Appliance(builder.TestName, builder.TestDescription,
+                -10.5m);
             });
         }
 
         [Fact]
         void AllowsEmptyNameAndDescription()
         {
-            var appliance = new Appliance("", "", 10.5m);
+            var builder = new ApplianceBuilder();
+            var appliance = builder.WithoutNameAndDescription();
 
             Assert.Equal("", appliance.Name);
             Assert.Equal("", appliance.Description);
