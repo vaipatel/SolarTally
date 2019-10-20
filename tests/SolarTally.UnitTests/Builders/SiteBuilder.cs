47acb625cf0e59a0f8ab5347ac57c1429f0c777f
vaipatel <vai.pateln@gmail.com>
2019-10-20 19:10:29 -0400

    Add a SiteBuilder

---
+++ tests/SolarTally.UnitTests/Builders/SiteBuilder.cs
@@ -0,0 +1,28 @@
+using System;
+using SolarTally.Domain.Entities;
+
+namespace SolarTally.UnitTests.Builders
+{
+    public class SiteBuilder
+    {
+        private Site _site;
+        public string TestName => "Mac's convenience";
+        public int TestNumSolarHours => 8;
+
+        public SiteBuilder()
+        {
+            _site = new Site(TestName, TestNumSolarHours);
+        }
+
+        public Site Build()
+        {
+            return _site;
+        }
+
+        public Site WithoutName()
+        {
+            _site = new Site("", TestNumSolarHours);
+            return _site;
+        }
+    }
+}
