27adffca58588ed2487e912746c1876aa582d8bc
vaipatel <vai.pateln@gmail.com>
2019-10-19 22:15:15 -0400

    Add private and parametered ctors to Appliance

--- src/SolarTally.Domain/Entities/Appliance.cs
+++ src/SolarTally.Domain/Entities/Appliance.cs
@@ -1,4 +1,5 @@
 using SolarTally.Domain.Common;
+using Ardalis.GuardClauses;
 
 namespace SolarTally.Domain.Entities
 {
@@ -16,5 +17,20 @@ namespace SolarTally.Domain.Entities
         public decimal DefaultPowerConsumption { get; set; }
 
         public int ApplianceUsageId { get; private set; }
+
+        private Appliance()
+        {
+            // Apparently required by EF
+        }
+
+        public Appliance(string name, string description,
+            decimal defaultPowerConsumption)
+        {
+            Guard.Against.LessThan(defaultPowerConsumption,
+                nameof(defaultPowerConsumption), 0);
+            Name = name;
+            Description = description;
+            DefaultPowerConsumption = defaultPowerConsumption;
+        }
     }
 }
