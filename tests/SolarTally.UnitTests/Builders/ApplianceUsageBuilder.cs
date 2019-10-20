042e99e08d896381c272d7c7d532ed012dee1b4b
vaipatel <vai.pateln@gmail.com>
2019-10-20 01:20:56 -0400

    Add an ApplianceUsageBuilder for tests

---
+++ tests/SolarTally.UnitTests/Builders/ApplianceUsageBuilder.cs
@@ -0,0 +1,31 @@
+using System;
+using SolarTally.Domain.Entities;
+
+namespace SolarTally.UnitTests.Builders
+{
+    public class ApplianceUsageBuilder
+    {
+        private ApplianceUsage _applianceUsage;
+
+        private Appliance _appliance;
+        public Appliance TestAppliance => _appliance;
+        public int TestQuantity => 2;
+        public decimal TestPowerConsumption => 20.5m;
+        public int TestNumHours => 3;
+        public int TestPercentHrsOnSolar => 1;
+        public bool TestEnabled => true;
+
+        public ApplianceUsageBuilder()
+        {
+            _appliance = new ApplianceBuilder().Build();
+            _applianceUsage = new ApplianceUsage(TestAppliance, TestQuantity,
+                TestPowerConsumption, TestNumHours, TestPercentHrsOnSolar,
+                TestEnabled);
+        }
+
+        public ApplianceUsage Build()
+        {
+            return _applianceUsage;
+        }
+    }
+}
