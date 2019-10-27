6fa302ac78a82ca84479eb2f0bc63afcb21076e1
vaipatel <vai.pateln@gmail.com>
2019-10-27 05:11:35 -0400

    Add some interfaces in Application

---
+++ src/SolarTally.Application/Common/Interfaces/ISolarTallyDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using SolarTally.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SolarTally.Application.Common.Interfaces
+{
+    public interface ISolarTallyDbContext
+    {
+        DbSet<Site> Sites { get; set; }
+        DbSet<Consumption> Consumptions { get; set; }
+        DbSet<ApplianceUsage> ApplianceUsages { get; set; }
+        DbSet<Appliance> Appliances { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
