8a658ceb9ab17584dfbf5c898b65f1f0373066a9
Vai Patel <vai.pateln@gmail.com>
2019-10-26 20:00:00 -0400

    Add a ConsumptionTotal value object
    This just directly takes in a Consumption for simplicity.
    Can't see too many side-effects.

---
+++ src/SolarTally.Domain/ValueObjects/ConsumptionTotal.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using SolarTally.Domain.Common;
+using SolarTally.Domain.Entities;
+
+namespace SolarTally.Domain.ValueObjects
+{
+    /// <summary>
+    /// Encapsulates the calculated totals for a single ApplianceUsage record,
+    /// given the quantity, power consumption in watts and hrs of operations.
+    /// </summary>
+    public class ConsumptionTotal : ValueObject
+{
+    public decimal TotalPowerConsumption { get; private set; }
+    public decimal TotalEnergyConsumption { get; private set; }
+
+    private ConsumptionTotal() { }
+
+    public ConsumptionTotal(Consumption consumption)
+    {
+        TotalPowerConsumption = 0;
+        TotalEnergyConsumption = 0;
+        foreach(var au in consumption.ApplianceUsages)
+        {
+            TotalPowerConsumption += 
+                au.ApplianceUsageTotal.TotalPowerConsumption;
+            TotalEnergyConsumption +=
+                au.ApplianceUsageTotal.TotalEnergyConsumption;
+        }
+    }
+
+    protected override IEnumerable<object> GetAtomicValues()
+    {
+        // Using a yield return statement to return each element one at a time
+        yield return TotalPowerConsumption;
+        yield return TotalEnergyConsumption;
+    }
+}
+}
