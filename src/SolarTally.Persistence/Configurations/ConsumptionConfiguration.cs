e03c91b728b6456606619cd33d19d6be557fde0c
Vai Patel <vai.pateln@gmail.com>
2019-10-27 17:00:09 -0400

    Add Consumption config

---
+++ src/SolarTally.Persistence/Configurations/ConsumptionConfiguration.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SolarTally.Domain.Entities;
+
+namespace SolarTally.Persistence.Configurations
+{
+    public class ConsumptionConfiguration :
+        IEntityTypeConfiguration<Consumption>
+    {
+        public void Configure(EntityTypeBuilder<Consumption>
+            consumptionConfiguration)
+        {   
+            consumptionConfiguration.Ignore(b => b.DomainEvents);
+
+            // Don't have a reference back to our Consumption
+            // See https://docs.microsoft.com/en-us/ef/core/modeling/relationships#single-navigation-property
+            consumptionConfiguration
+                .HasMany<ApplianceUsage>(c => c.ApplianceUsages);
+
+            consumptionConfiguration.OwnsOne(c => c.ConsumptionTotal);
+        }
+    }
+}
