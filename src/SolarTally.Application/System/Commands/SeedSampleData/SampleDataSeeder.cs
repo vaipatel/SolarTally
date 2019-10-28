c06e3c9469f935f6f6991d60756d4226afe0c82a
Vai Patel <vai.pateln@gmail.com>
2019-10-28 16:07:39 -0400

    Mv files to adhere to cmd folder structure

--- src/SolarTally.Application/System/Commands/SeedSampleData/SampleDataSeeder.cs
+++ src/SolarTally.Application/System/Commands/SeedSampleData/SampleDataSeeder.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace SolarTally.Application
+namespace SolarTally.Application.System.Commands.SeedSampleData
 {
     public class SampleDataSeeder
     {
