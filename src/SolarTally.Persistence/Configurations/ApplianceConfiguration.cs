a3a23b164ad5271726c304b505cfbe3f8f6cd40b
vaipatel <vai.pateln@gmail.com>
2019-10-27 22:56:11 -0400

    Add Appliance config

---
+++ src/SolarTally.Persistence/Configurations/ApplianceConfiguration.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SolarTally.Domain.Entities;
+
+namespace SolarTally.Persistence.Configurations
+{
+    public class ApplianceConfiguration : IEntityTypeConfiguration<Appliance>
+    {
+        public void Configure(
+            EntityTypeBuilder<Appliance> applianceConfiguration)
+        {
+            applianceConfiguration.Ignore(b => b.DomainEvents);
+
+            applianceConfiguration.Property(a => a.Name)
+                .HasMaxLength(50)
+                .HasDefaultValue("Appliance Name")
+                .IsRequired();
+            
+            applianceConfiguration.Property(a => a.Description)
+                .HasMaxLength(50)
+                .HasDefaultValue("Appliance Description")
+                .IsRequired(false);
+
+            applianceConfiguration.Property(a => a.DefaultPowerConsumption)
+                .HasColumnType("decimal(18,2)")
+                .HasDefaultValue<decimal>(30.00)
+                .IsRequired();
+        }
+    }
+}
