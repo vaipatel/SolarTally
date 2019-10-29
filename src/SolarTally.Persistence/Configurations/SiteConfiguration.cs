2e1b58405d803daae272ba0355bf8df95da30bf4
Vai Patel <vai.pateln@gmail.com>
2019-10-28 21:38:40 -0400

    Specify Site FK to make it parent
    Gets rid of this error when migrating -
    
    System.AggregateException : One or more errors occurred. (The child/dependent side could not be determined for the one-to-one relationship between 'Consumption.Site' and 'Site.Consumption'. To identify the child/dependent side of the relationship, configure the foreign key property. If these navigations should not be part of the same relationship configure them without specifying the inverse. See http://go.microsoft.com/fwlink/?LinkId=724062 for more details.)

--- src/SolarTally.Persistence/Configurations/SiteConfiguration.cs
+++ src/SolarTally.Persistence/Configurations/SiteConfiguration.cs
@@ -22,8 +22,31 @@ namespace SolarTally.Persistence.Configurations
                 .HasDefaultValue<int>(8)
                 .IsRequired();
 
+            // Note: If I don't configure the HasForeignKey<Consumption>(), then
+            // I'll get the following error
+            // -------
+            // System.AggregateException : One or more errors occurred. (The 
+            // child/dependent side could not be determined for the one-to-one 
+            // relationship between 'Consumption.Site' and 'Site.Consumption'. 
+            // To identify the child/dependent side of the relationship, 
+            // configure the foreign key property. If these navigations should 
+            // not be part of the same relationship configure them without 
+            // specifying the inverse. 
+            // See http://go.microsoft.com/fwlink/?LinkId=724062 for more 
+            // details.)
+            // -------
+            // Note (cont.): Basically EF Core is confused about which is the
+            // child in the one-to-one relationship between Site and 
+            // Consumption. It will resolve this confusion when it can detect a
+            // FK configuration, as we have done with the HasForeignKey() call
+            // below.
+            // See the link in the error.
+            // Also see the question https://stackoverflow.com/q/51808912 to
+            // understand which one is dubbed the child by EF Core.
+
             siteConfiguration.HasOne(s => s.Consumption)
                 .WithOne(c => c.Site)
+                .HasForeignKey<Consumption>(c => c.SiteId)
                 .OnDelete(DeleteBehavior.Cascade)
                 .IsRequired(true);
         }
