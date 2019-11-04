64ceb56f40ee64e702a6e9ad1447656a3efc035f
Vai Patel <vai.pateln@gmail.com>
2019-11-04 14:36:59 -0500

    AddPersistence

--- src/SolarTally.WebUI_Ng/Startup.cs
+++ src/SolarTally.WebUI_Ng/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SolarTally.Persistence;
 
 namespace SolarTally.WebUI_Ng
 {
@@ -20,6 +21,9 @@ namespace SolarTally.WebUI_Ng
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add Persistence objects (basically the DbContext)
+            services.AddPersistence(Configuration);
+
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
