d42fa3b22b502925efd323f3693b005b5d5206ed
Vai Patel <vai.pateln@gmail.com>
2019-11-04 22:42:03 -0500

    Add a synchronous seeder
    I think MediatR's async execution is not sitting well with Npgsql.
    
    I get this error message -
    Npgsql.NpgsqlException (0x80004005): Received backend message ParseComplete while expecting ReadyForQueryMessage. Please file a bug.
    
    Not sure what to do here.

---
+++ src/SolarTally.WebUI_Ng/CustomSeeder.cs
@@ -0,0 +1,94 @@
+using SolarTally.Application.Common.Interfaces;
+using SolarTally.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SolarTally.WebUI_Ng
+{
+    public class CustomSeeder
+    {
+        private readonly ISolarTallyDbContext _context;
+
+        private readonly Dictionary<int, Site> Sites =
+            new Dictionary<int, Site>();
+        private readonly Dictionary<int, Consumption> Consumptions =
+            new Dictionary<int, Consumption>();
+        private readonly Dictionary<int, ApplianceUsage> ApplianceUsages =
+            new Dictionary<int, ApplianceUsage>();
+        private readonly Dictionary<int, Appliance> Appliances =
+            new Dictionary<int, Appliance>();
+
+        public CustomSeeder(ISolarTallyDbContext context)
+        {
+            _context = context;
+        }
+        
+        public void SeedAll()
+        {
+            MakeAppliances();
+            MakeSites();
+            MakeConsumptions();
+            MakeApplianceUsages();
+
+            foreach(var appliance in Appliances.Values)
+            {
+                _context.Appliances.Add(appliance);
+            }
+
+            foreach(var site in Sites.Values)
+            {
+                _context.Sites.Add(site);
+                _context.Consumptions.Add(site.Consumption);
+                foreach(var consumption in Consumptions.Values)
+                {
+                    foreach(var au in consumption.ApplianceUsages)
+                    {
+                        _context.ApplianceUsages.Add(au);
+                    }
+                }
+            }
+            _context.SaveChanges(true);
+        }
+
+        public void MakeAppliances()
+        {
+            Appliances.Add(1, new Appliance("LED", "An LED bulb", 20));
+            Appliances.Add(2, new Appliance("TV", "A 32 inch LCD TV", 80));
+            Appliances.Add(3, new Appliance("Heater", "", 300));
+        }
+
+        public void MakeSites()
+        {
+            Sites.Add(1, new Site("Campbell Residence", 7));
+            Sites.Add(2, new Site("St. Mary School", 9));
+            Sites.Add(3, new Site("OTI Imaging Center", 8));
+        }
+
+        public void MakeConsumptions()
+        {
+            foreach(var site in Sites)
+            {
+                Consumptions.Add(site.Key, site.Value.Consumption);
+            }
+        }
+
+        public void MakeApplianceUsages()
+        {
+            Consumptions[1].AddApplianceUsage(Appliances[1]);
+            Consumptions[1].AddApplianceUsage(Appliances[2]);
+
+            Consumptions[2].AddApplianceUsage(Appliances[2]);
+            Consumptions[2].ApplianceUsages.Last().SetQuantity(10);
+            Consumptions[2].ApplianceUsages.Last().SetNumHours(5);
+            Consumptions[2].AddApplianceUsage(Appliances[3]);
+
+            Consumptions[3].AddApplianceUsage(Appliances[1]);
+            Consumptions[3].AddApplianceUsage(Appliances[2]);
+            Consumptions[3].AddApplianceUsage(Appliances[3]);
+        }
+
+    }
+}
