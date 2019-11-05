b2bc6b8f5fc2d25210c0bccb9918afa54fb109d3
Vai Patel <vai.pateln@gmail.com>
2019-11-04 22:49:33 -0500

    Run seeder in Program
    Since I'm calling Migrate(), I think I'll have to make sure that migrations have been run.
    That can be done by
    1. cd src/SolarTally.Persistence
    2. dotnet ef migrations add InitialCreate
    3. dotnet ef database update
    
    To remove the migrations, I can
    1. Delete the solartallydb from Postgres (can use PgAdmin4)
    2. dotnet ef migrations remove

--- src/SolarTally.WebUI_Ng/Program.cs
+++ src/SolarTally.WebUI_Ng/Program.cs
@@ -1,11 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SolarTally.Application.System.Commands.SeedSampleData;
+using SolarTally.Persistence;
 
 namespace SolarTally.WebUI_Ng
 {
@@ -13,7 +19,35 @@ namespace SolarTally.WebUI_Ng
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogInformation("Trying to init database");
+
+                try
+                {
+                    var solarTallyDbContext = services
+                        .GetRequiredService<SolarTallyDbContext>();
+                    solarTallyDbContext.Database.Migrate();
+                    
+                    // var mediator = services.GetRequiredService<IMediator>();
+                    // mediator.Send(new SeedSampleDataCommand(),
+                    //     CancellationToken.None);
+                    
+                    var seeder = new CustomSeeder(solarTallyDbContext);
+                    seeder.SeedAll();
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, @"An error occurred while initializing 
+                        the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
