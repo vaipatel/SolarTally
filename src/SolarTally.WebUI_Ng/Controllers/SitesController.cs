0738cad7e26a704bf868b2d3d3c692538fa64aa0
vaipatel <vai.pateln@gmail.com>
2019-11-07 01:40:15 -0500

    Init SitesController

---
+++ src/SolarTally.WebUI_Ng/Controllers/SitesController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SolarTally.Application.Sites.Queries.Dtos;
+using SolarTally.Application.Sites.Queries.GetSitePartialDtosList;
+using SolarTally.Application.Sites.Queries.GetSitesList;
+using System.Threading.Tasks;
+
+namespace SolarTally.WebUI_Ng.Controllers
+{
+    public class SitesController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<SitePartialDtosListVm>> GetAll()
+        {
+            return Ok(await Mediator.Send(new GetSitePartialDtosListQuery()));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<SitesListVm>> GetAllFull()
+        {
+            return Ok(await Mediator.Send(new GetSitesListQuery()));
+        }
+
+        // [HttpPost]
+        // [ProducesResponseType(StatusCodes.Status200OK)]
+        // [ProducesDefaultResponseType]
+        // public async Task<IActionResult> Upsert(UpsertSiteCommand command)
+        // {
+        //     var id = await Mediator.Send(command);
+
+        //     return Ok(id);
+        // }
+
+        // [HttpDelete("{id}")]
+        // [ProducesResponseType(StatusCodes.Status204NoContent)]
+        // [ProducesResponseType(StatusCodes.Status404NotFound)]
+        // public async Task<IActionResult> Delete(int id)
+        // {
+        //     await Mediator.Send(new DeleteSiteCommand { Id = id });
+
+        //     return NoContent();
+        // }
+    }
+}
