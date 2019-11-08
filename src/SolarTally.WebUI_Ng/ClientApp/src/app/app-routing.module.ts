05aba9d202b292c83f2de27e453bd84257cf1131
Vai Patel <vai.pateln@gmail.com>
2019-11-07 23:03:33 -0500

    Add sites to route

--- src/SolarTally.WebUI_Ng/ClientApp/src/app/app-routing.module.ts
+++ src/SolarTally.WebUI_Ng/ClientApp/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SitesComponent } from './sites/sites.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sites', component: SitesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
