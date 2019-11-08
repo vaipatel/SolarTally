12cd0b593582e791b59841f26621107db86c1ffd
Vai Patel <vai.pateln@gmail.com>
2019-11-08 00:22:30 -0500

    Add material, animations
    using deeppurple-amber theme

--- src/SolarTally.WebUI_Ng/ClientApp/src/app/app.module.ts
+++ src/SolarTally.WebUI_Ng/ClientApp/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SitesComponent } from './sites/sites.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
