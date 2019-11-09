2db64be48aac155dbb9d5ee484e7c8b2ee3be92b
Vai Patel <vai.pateln@gmail.com>
2019-11-08 23:22:48 -0500

    Add srvc func to Get SiteBriefs list from api

--- src/SolarTally.WebUI_Ng/ClientApp/src/app/sites/shared/site.service.ts
+++ src/SolarTally.WebUI_Ng/ClientApp/src/app/sites/shared/site.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Site } from './site';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Site, SiteBrief, SiteBriefLst } from './site';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SiteService {
 
-  constructor() { }
+  private sitesUrl = "api/sites/getall";
+
+  constructor(private http: HttpClient) { }
+
+  getSites(): Observable<SiteBriefLst> {
+    return this.http.get<SiteBriefLst>(this.sitesUrl);
+  }
 }
