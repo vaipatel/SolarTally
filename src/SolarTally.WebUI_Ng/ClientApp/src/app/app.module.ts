7d61a5d906ebd3ca53d5299f22d24f2a3736e36a
Vai Patel <vai.pateln@gmail.com>
2019-11-09 04:43:47 -0500

    Import MatTableModule

--- src/SolarTally.WebUI_Ng/ClientApp/src/app/app.module.ts
+++ src/SolarTally.WebUI_Ng/ClientApp/src/app/app.module.ts
@@ -15,6 +15,7 @@ import {
   MatIconModule,
   MatListModule,
   MatMenuModule,
+  MatTableModule,
   MatToolbarModule
 } from '@angular/material';
 
@@ -35,6 +36,7 @@ import {
     MatIconModule,
     MatListModule,
     MatMenuModule,
+    MatTableModule,
     MatToolbarModule
   ],
   providers: [],
