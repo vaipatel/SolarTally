a7168dc87b7ff858440c1effd6751c864f91bc21
Vai Patel <vai.pateln@gmail.com>
2019-11-08 23:20:42 -0500

    Complete Site, add SiteBrief

--- src/SolarTally.WebUI_Ng/ClientApp/src/app/sites/shared/site.ts
+++ src/SolarTally.WebUI_Ng/ClientApp/src/app/sites/shared/site.ts
@@ -1,5 +1,21 @@
+import { ConsumptionTotal } from 'src/app/shared/consumption-total';
+import { Address } from 'src/app/shared/address';
+
 export class Site {
     id: number;
     name: string;
     numSolarHours: number;
+    mainAddress: Address;
+    consumptionTotal: ConsumptionTotal;
+}
+
+export class SiteBrief {
+    id: number;
+    name: string;
+    mainAddressCity: string;
+    consumptionTotal: ConsumptionTotal;
+}
+
+export class SiteBriefLst {
+    items: SiteBrief[];
 }
