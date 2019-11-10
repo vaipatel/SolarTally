ed87c2cc3dbb9eb5dda8f65866d2f04a80cc439d
vaipatel <vai.pateln@gmail.com>
2019-11-10 03:19:06 -0500

    Invoke appliance usages fetch on init

--- src/SolarTally.WebUI_Ng/ClientApp/src/app/consumption/consumption.component.ts
+++ src/SolarTally.WebUI_Ng/ClientApp/src/app/consumption/consumption.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ConsumptionService } from './shared/consumption.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { ApplianceUsageLst, ApplianceUsage } from '../shared/appliance-usage';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-consumption',
@@ -8,9 +13,29 @@ import { ConsumptionService } from './shared/consumption.service';
 })
 export class ConsumptionComponent implements OnInit {
 
-  constructor(private consumptionSrvc: ConsumptionService) { }
+  private auLst$: Observable<ApplianceUsageLst>;
+  auList: ApplianceUsage[];
+
+  constructor(
+    private route: ActivatedRoute,
+    private consumptionSrvc: ConsumptionService) { }
 
   ngOnInit() {
+    // Read route params
+    this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.auLst$ = this.consumptionSrvc
+          .getApplianceUsagesForConsumption(params.get('id'))
+      )
+    ).subscribe((resp: ApplianceUsageLst) => {
+      if (!resp) {
+        console.log("No Appliance Usages founded.");
+      }
+      this.auList = resp.items;
+      console.log(this.auList);
+    }, (error: HttpErrorResponse) => console.log(
+      "ApplianceUsages not found - Error " + error.status
+    ));
   }
 
 }
