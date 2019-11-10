1f30dda4a4dfa268a5bf4633e2a77df5edd87269
Vai Patel <vai.pateln@gmail.com>
2019-11-10 00:23:13 -0500

    Import httpclient

--- src/SolarTally.WebUI_Ng/ClientApp/src/app/consumption/shared/consumption.service.ts
+++ src/SolarTally.WebUI_Ng/ClientApp/src/app/consumption/shared/consumption.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConsumptionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
