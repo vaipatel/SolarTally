a2df515ab6a5a2fd5202350df4c5ede027419e51
vaipatel <vai.pateln@gmail.com>
2019-11-10 03:18:36 -0500

    Add srvc func to get AUs by consumption id

--- src/SolarTally.WebUI_Ng/ClientApp/src/app/consumption/shared/consumption.service.ts
+++ src/SolarTally.WebUI_Ng/ClientApp/src/app/consumption/shared/consumption.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { ApplianceUsageLst } from 'src/app/shared/appliance-usage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConsumptionService {
+  
+  private auByIdUrl = "api/applianceusages/getforconsumption/";
 
   constructor(private http: HttpClient) { }
+
+  getApplianceUsagesForConsumption(id: string): Observable<ApplianceUsageLst>{
+    return this.http.get<ApplianceUsageLst>(this.auByIdUrl + id);
+  }
 }
