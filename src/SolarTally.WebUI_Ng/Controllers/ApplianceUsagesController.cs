e235433c3ad40558a7ed5155efd2a5154ba4420b
vaipatel <vai.pateln@gmail.com>
2019-11-11 02:33:46 -0500

    Rem useless (for now) query

--- src/SolarTally.WebUI_Ng/Controllers/ApplianceUsagesController.cs
+++ src/SolarTally.WebUI_Ng/Controllers/ApplianceUsagesController.cs
@@ -3,19 +3,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SolarTally.Application.ApplianceUsages.Queries.Dtos;
 using SolarTally.Application.ApplianceUsages.Queries.GetApplianceUsagesById;
-using SolarTally.Application.ApplianceUsages.Queries.GetApplianceUsagesList;
 using System.Threading.Tasks;
 
 namespace SolarTally.WebUI_Ng.Controllers
 {
     public class ApplianceUsagesController : BaseController
     {
-        [HttpGet]
-        public async Task<ActionResult<ApplianceUsagesListDto>> GetAll()
-        {
-            return Ok(await Mediator.Send(new GetApplianceUsagesListQuery()));
-        }
-
         [HttpGet("{id}")]
         public async Task<ActionResult<ApplianceUsagesListDto>> GetForConsumption(int id)
         {
