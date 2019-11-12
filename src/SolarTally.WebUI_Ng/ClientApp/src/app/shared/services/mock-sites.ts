fc488086b8fc139ec2cfa71c5ee8f10c9060cc92
vaipatel <vai.pateln@gmail.com>
2019-11-11 23:25:24 -0500

    Finish mv in [0e55b63]

---
+++ src/SolarTally.WebUI_Ng/ClientApp/src/app/shared/services/mock-sites.ts
@@ -0,0 +1,40 @@
+import { Site } from '../dtos/site';
+
+export const SITES: Site[] = [
+    { 
+        id: 1, name: "Great Lakes Vet", numSolarHours: 6, mainAddress: { 
+            street: "0 Sandalwood Pkwy", 
+            city: "Brampton", 
+            state: "ON", 
+            country: "CAN", 
+            zipCode: "L6N8P0" },
+        consumptionTotal: { 
+            totalPowerConsumption: 100, 
+            totalEnergyConsumption: 500
+        }
+    },
+    { 
+        id: 1, name: "First Choice", numSolarHours: 9, mainAddress: { 
+            street: "0 Front St", 
+            city: "Toronto", 
+            state: "ON", 
+            country: "CAN", 
+            zipCode: "M6N8O0" },
+        consumptionTotal: { 
+            totalPowerConsumption: 320, 
+            totalEnergyConsumption: 1600
+        }
+    },
+    { 
+        id: 1, name: "Aberfoyle Mill", numSolarHours: 5, mainAddress: { 
+            street: "0 Brock Rd", 
+            city: "Guelph", 
+            state: "ON", 
+            country: "CAN", 
+            zipCode: "N0O1P2" },
+        consumptionTotal: { 
+            totalPowerConsumption: 1040, 
+            totalEnergyConsumption: 4160
+        }
+    }
+]
---
+++ src/SolarTally.WebUI_Ng/ClientApp/src/app/shared/services/site.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Site, SiteBrief, SiteBriefsLst } from '../dtos/site';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SiteService {
+
+  private sitesUrl = "api/sites/getall";
+  private siteDetailUrl = "api/sites/getdetail/";
+
+  constructor(private http: HttpClient) { }
+
+  getSites(): Observable<SiteBriefsLst> {
+    return this.http.get<SiteBriefsLst>(this.sitesUrl);
+  }
+
+  getSiteDetail(id: string): Observable<Site> {
+    return this.http.get<Site>(this.siteDetailUrl + id);
+  }
+}
