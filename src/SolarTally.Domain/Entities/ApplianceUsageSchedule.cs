672df8098ed5f29b95dd96d5d5f33ff6dab0dde0
Vai Patel <vai.pateln@gmail.com>
2019-11-14 23:45:16 -0500

    Intro SetPeakSolarHours with logic in ApplianceUsageSchedule

--- src/SolarTally.Domain/Entities/ApplianceUsageSchedule.cs
+++ src/SolarTally.Domain/Entities/ApplianceUsageSchedule.cs
@@ -24,12 +24,85 @@ namespace SolarTally.Domain.Entities
             _usageIntervals = new List<UsageTimeInterval>();
         }
 
-        public void SetPeakSolarInterval(TimeInterval ti)
+        public void SetPeakSolarInterval(
+            TimeInterval ti, bool addIfEmpty = false
+        )
         {
-            var newTI = new TimeInterval(ti.Start.Hours, ti.Start.Minutes,
-                ti.End.Hours, ti.End.Minutes);
-            var uTI = new UsageTimeInterval(newTI, UsageKind.UsingSolar);
-            _usageIntervals.Add(uTI);
+            int startHr = ti.Start.Hours, startMin = ti.Start.Minutes;
+            int endHr   = ti.End.Hours,   endMin   = ti.End.Minutes;
+
+            if (addIfEmpty)
+            {
+                if (_usageIntervals.Count == 0)
+                {
+                    var newTI = new TimeInterval(startHr, startMin, endHr, endMin);
+                    var uTI = new UsageTimeInterval(newTI, UsageKind.UsingSolar);
+                    _usageIntervals.Add(uTI);
+                    return;
+                }
+            }
+
+            bool lowerDone = false;
+            bool upperDone = false;
+            for(int i = 0; i < _usageIntervals.Count; ++i)
+            {
+                var uTI = _usageIntervals[i].TimeInterval;
+                var usage = _usageIntervals[i].UsageKind;
+
+                // Ensure we're just dealing with solar usage intervals.
+                if (usage != UsageKind.UsingSolar)
+                {
+                    continue;
+                }
+
+                // If we have a solar usage interval that is completely outside
+                // the new peak solar interval, then convert it to mains usage.
+                if (uTI.Start > ti.End)
+                {
+                    var newTI = new TimeInterval(
+                        uTI.Start.Hours, uTI.Start.Minutes,
+                        uTI.End.Hours, uTI.End.Minutes);
+                    _usageIntervals[i] = new UsageTimeInterval(newTI,
+                        UsageKind.UsingMains);
+                    continue;
+                }
+
+                // If we haven't trimmed the lower bound yet ..
+                if (!lowerDone)
+                {
+                    // .. and we have a solar usage interval starting before
+                    // the start of the peak solar interval
+                    if (uTI.Start < ti.Start)
+                    {
+                        // we gottu trim it!
+                        var newTI = new TimeInterval(
+                            startHr, startMin, uTI.End.Hours, uTI.End.Minutes
+                        );
+                        _usageIntervals[i] = new UsageTimeInterval(newTI);
+                        // since we're storing intervals in temporal order,
+                        // we won't need to do this again.
+                        lowerDone = true;
+                    }
+                }
+
+                // If we haven't trimmed the upper bound yet ..
+                if (!upperDone)
+                {
+                    // .. and we have a solar usage interval starting after
+                    // the end of the peak solar interval
+                    if (uTI.End > ti.End)
+                    {
+                        // we gottu trim it!
+                        var newTI = new TimeInterval(
+                            uTI.Start.Hours, uTI.Start.Minutes, endHr, endMin
+                        );
+                        _usageIntervals[i] = new UsageTimeInterval(newTI);
+                        // since we're storing intervals in temporal order,
+                        // we won't need to do this again.
+                        upperDone = true;
+                    }
+                }
+            }
         }
     }
 }
