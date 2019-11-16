b3aee74b069574077a8bb65dd284f7521147790e
Vai Patel <vai.pateln@gmail.com>
2019-11-16 16:56:13 -0500

    Add func to calc max power consumption
    Not correct. Need to work on it more.

--- src/SolarTally.Domain/ValueObjects/ConsumptionTotal.cs
+++ src/SolarTally.Domain/ValueObjects/ConsumptionTotal.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using System.Linq;
 using SolarTally.Domain.Common;
 using SolarTally.Domain.Entities;
+using SolarTally.Domain.Enumerations;
 
 namespace SolarTally.Domain.ValueObjects
 {
@@ -42,6 +44,101 @@ namespace SolarTally.Domain.ValueObjects
             yield return TotalOnSolarEnergyConsumption;
             yield return TotalOffSolarEnergyConsumption;
             yield return TotalEnergyConsumption;
+        }
+
+        private decimal GetMaxPowerConsumption(ref Consumption consumption)
+        {
+            // First get the total number of usage intervals
+            var idxes = new List<int>();
+            var counts = new List<int>();
+            var totalSteps = 0;
+            foreach(var au in consumption.ApplianceUsages)
+            {
+                var count = au.ApplianceUsageSchedule.UsageIntervals.Count;
+                totalSteps += count;
+                counts.Add(count);
+                idxes.Add(0);
+            }
+
+            var numAUs = consumption.ApplianceUsages.Count;
+            decimal currMaxPower = 0;
+            // While we haven't finished stepping thru all AU schedules
+            while (!idxes.SequenceEqual(counts))
+            {
+                // 1. Compare UsageTimeIntervals at idxes to get earliest
+                UsageTimeInterval currEarliest = null;
+                decimal currPowerSum = 0;
+                int lastAdvancedIdx = -1;
+                for (int auIdx = 0; auIdx < numAUs; ++auIdx)
+                {
+                    var idx = idxes[auIdx];
+                    
+                    // If idxes[auIdx] == counts[auIdx], this AU is over
+                    if (idx == counts[auIdx])
+                    {
+                        continue;
+                    }
+
+                    var auAtIdx = consumption.ApplianceUsages.ElementAt(auIdx); // Note: ElementAt inefficient
+                    var utiAtIdx = auAtIdx
+                        .ApplianceUsageSchedule.UsageIntervals.ElementAt(idx); // Note: ElementAt inefficient
+                    
+                    // We only care about Max Power Consumption for our
+                    // solar inverters.
+                    if (utiAtIdx.UsageKind != UsageKind.UsingSolar)
+                    {
+                        // 2. Advance 
+                        ++idxes[auIdx];
+                        lastAdvancedIdx = auIdx;
+                        continue;
+                    }
+
+                    // If earliest has not been set yet,
+                    if (currEarliest == null)
+                    {
+                        // Make this the currEarliest and compare back from next
+                        currEarliest = utiAtIdx;
+                        currPowerSum = auAtIdx.PowerConsumption; // TODO: Make it StartupPowerConsumption
+                        // 2. Advance 
+                        ++idxes[auIdx];
+                        lastAdvancedIdx = auIdx;
+                        continue;
+                    }
+                    else // Compare to prev
+                    {
+                        if (utiAtIdx.TimeInterval.Start < 
+                            currEarliest.TimeInterval.Start)
+                        {
+                            currEarliest = utiAtIdx;
+                            currPowerSum = auAtIdx.PowerConsumption;
+                            // 2. Advance 
+                            ++idxes[auIdx];
+                            // 2.1. Retreat last advanced idx because its UTI
+                            // happens later compared to the UTI at this idx
+                            --idxes[lastAdvancedIdx];
+                            lastAdvancedIdx = auIdx;
+                        }
+                        else if (utiAtIdx.TimeInterval.Start == 
+                            currEarliest.TimeInterval.Start)
+                        {
+                            currPowerSum += auAtIdx.PowerConsumption; // TODO: Make it StartupPowerConsumption
+                            // 2. Advance
+                            ++idxes[auIdx];
+                            lastAdvancedIdx = auIdx;
+                        }
+                        else
+                        {
+                            var prevIdx = idx - 1;
+                            var utiAtPrevIdx = auAtIdx
+                                .ApplianceUsageSchedule.UsageIntervals
+                                .ElementAt(prevIdx); // Note: ElementAt inefficient
+                            
+                        }
+                    }
+                }
+                currMaxPower = System.Math.Max(currMaxPower, currPowerSum);
+            }
+            return currMaxPower;
         }
     }
 }
