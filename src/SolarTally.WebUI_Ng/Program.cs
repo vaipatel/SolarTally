8ae2a01baf18418c2c3c8d83fb852be6f90ed9df
Vai Patel <vai.pateln@gmail.com>
2019-11-23 10:00:26 -0500

    Rem using

--- src/SolarTally.WebUI_Ng/Program.cs
+++ src/SolarTally.WebUI_Ng/Program.cs
@@ -10,7 +10,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using SolarTally.Application.System.Commands.SeedSampleData;
 using SolarTally.Persistence;
 
 namespace SolarTally.WebUI_Ng
