f96549bdf6f1ff162544335f4726e4e4b4460214
Vai Patel <vai.pateln@gmail.com>
2019-12-03 17:41:13 -0500

    Create ApplianceUsageStr that uses ApplianceUsageScheduleStr
    Also add Lst wrapper.
    
    Still client side.

--- src/SolarTally.WebUI_Ng/ClientApp/src/app/shared/dtos/appliance-usage.ts
+++ src/SolarTally.WebUI_Ng/ClientApp/src/app/shared/dtos/appliance-usage.ts
@@ -1,6 +1,6 @@
 import { Appliance } from './appliance';
 import { ApplianceUsageTotal } from './appliance-usage-total';
-import { ApplianceUsageSchedule } from './appliance-usage-schedule';
+import { ApplianceUsageSchedule, ApplianceUsageScheduleStr } from './appliance-usage-schedule';
 
 export class ApplianceUsage {
     id: number;
@@ -18,4 +18,22 @@ export class ApplianceUsage {
 
 export class ApplianceUsageLst {
     items: ApplianceUsage[];
+}
+
+export class ApplianceUsageStr {
+    id: number;
+    applianceDto: Appliance;
+    quantity: number;
+    powerConsumption: number;
+    numHoursOnSolar: number;
+    numHoursOffSolar: number;
+    numHours: number;
+    enabled: boolean;
+    consumptionId: number;
+    applianceUsageTotal: ApplianceUsageTotal;
+    applianceUsageScheduleDto: ApplianceUsageScheduleStr;
+}
+
+export class ApplianceUsageLstStr {
+    items: ApplianceUsageStr[];
 }
