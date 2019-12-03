e26a108e9409098fb75ac7f4316d639aa26c6b5c
Vai Patel <vai.pateln@gmail.com>
2019-12-03 17:30:44 -0500

    Add an abrv TimeInterval with abrv TimeSpans

--- src/SolarTally.WebUI_Ng/ClientApp/src/app/shared/dtos/time-interval.ts
+++ src/SolarTally.WebUI_Ng/ClientApp/src/app/shared/dtos/time-interval.ts
@@ -1,8 +1,15 @@
-import { TimeSpan } from './time-span';
+import { TimeSpan, TimeSpanAbrv } from './time-span';
 
 export class TimeInterval
 {
     start: TimeSpan;
     end: TimeSpan;
     difference: TimeSpan;
+}
+
+export class TimeIntervalAbrv
+{
+    start: TimeSpanAbrv;
+    end: TimeSpanAbrv;
+    difference: TimeSpanAbrv;
 }
