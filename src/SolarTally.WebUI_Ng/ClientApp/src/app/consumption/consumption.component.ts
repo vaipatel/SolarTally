673fd245645ded277061d87f36b3d05e1028c96b
Vai Patel <vai.pateln@gmail.com>
2019-12-03 18:58:37 -0500

    Use ApplianceUsageStr, ApplianceUsageLstStr in consumption component

--- src/SolarTally.WebUI_Ng/ClientApp/src/app/consumption/consumption.component.ts
+++ src/SolarTally.WebUI_Ng/ClientApp/src/app/consumption/consumption.component.ts
@@ -3,7 +3,7 @@ import { ApiService } from '../shared/services/api.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-import { ApplianceUsageLst, ApplianceUsage } from '../shared/dtos/appliance-usage';
+import { ApplianceUsageLst, ApplianceUsage, ApplianceUsageLstStr, ApplianceUsageStr } from '../shared/dtos/appliance-usage';
 import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
@@ -13,42 +13,65 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class ConsumptionComponent implements OnInit {
 
-  private auList$: Observable<ApplianceUsageLst>;
+  // private auList$: Observable<ApplianceUsageLst>;
+  private auListStr$: Observable<ApplianceUsageLstStr>;
   idParam = { id: "" };
-  auList: ApplianceUsage[];
+  // auList: ApplianceUsage[];
+  auListStr: ApplianceUsageStr[];
 
   constructor(
     private route: ActivatedRoute,
     private api: ApiService) { }
 
+  // ngOnInit() {
+  //   // Read route params
+  //   this.route.paramMap.pipe(
+  //     switchMap((params: ParamMap) => {
+  //       this.idParam.id = params.get('id');
+  //       this.auList$ =
+  //         this.api.getApplianceUsagesForConsumption(this.idParam.id);
+  //       return this.auList$;
+  //     })
+  //   ).subscribe((resp: ApplianceUsageLst) => {
+  //     if (!resp) {
+  //       console.log("No Appliance Usages founded.");
+  //     }
+  //     this.auList = resp.items;
+  //     console.log(this.auList);
+  //   }, (error: HttpErrorResponse) => console.log(
+  //     "ApplianceUsages not found - Error " + error.status
+  //   ));
+  // }
   ngOnInit() {
     // Read route params
     this.route.paramMap.pipe(
       switchMap((params: ParamMap) => {
         this.idParam.id = params.get('id');
-        this.auList$ =
-          this.api.getApplianceUsagesForConsumption(this.idParam.id);
-        return this.auList$;
+        this.auListStr$ =
+          this.api.getApplianceUsagesForConsumptionStr(this.idParam.id);
+          return this.auListStr$;
+
       })
-    ).subscribe((resp: ApplianceUsageLst) => {
+    ).subscribe((resp: ApplianceUsageLstStr) => {
       if (!resp) {
         console.log("No Appliance Usages founded.");
       }
-      this.auList = resp.items;
+      this.auListStr = resp.items;
+      console.log(this.auListStr);
     }, (error: HttpErrorResponse) => console.log(
       "ApplianceUsages not found - Error " + error.status
     ));
   }
 
-  addAppliance() {
-    console.log("Will add appliance");
-    this.api.mockAddToConsumption(this.idParam.id)
-    .subscribe(response => {
-      console.log("Response is:");
-      console.log(response);
-      this.auList.push(response);
-    });
-  }
+  // addAppliance() {
+  //   console.log("Will add appliance");
+  //   this.api.mockAddToConsumption(this.idParam.id)
+  //   .subscribe(response => {
+  //     console.log("Response is:");
+  //     console.log(response);
+  //     this.auList.push(response);
+  //   });
+  // }
 
   removeApplianceUsage(au: ApplianceUsage) {
     console.log("Will remove appliance usage " + au.id);
