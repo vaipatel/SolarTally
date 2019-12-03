185615d0db7deedac54d8949ec42f76be78e767d
Vai Patel <vai.pateln@gmail.com>
2019-12-03 17:33:14 -0500

    Create a str ApplianceUsageSchedule using UsageTimeIntervalStr
    Still client side.

--- src/SolarTally.WebUI_Ng/ClientApp/src/app/shared/dtos/appliance-usage-schedule.ts
+++ src/SolarTally.WebUI_Ng/ClientApp/src/app/shared/dtos/appliance-usage-schedule.ts
@@ -1,4 +1,4 @@
-import { UsageTimeInterval } from './usage-time-interval';
+import { UsageTimeInterval, UsageTimeIntervalStr } from './usage-time-interval';
 import { TimeSpan } from './time-span';
 
 export class ApplianceUsageSchedule
@@ -6,6 +6,17 @@ export class ApplianceUsageSchedule
     usageIntervals: UsageTimeInterval[];
     totalTimeOnSolar: TimeSpan;
     totalTimeOffSolar: TimeSpan;
+    hoursOnSolar: number;
+    hoursOffSolar: number;
+    hours: number;
+}
+
+
+export class ApplianceUsageScheduleStr
+{
+    usageIntervals: UsageTimeIntervalStr[];
+    totalTimeOnSolar: string;
+    totalTimeOffSolar: string;
     hoursOnSolar: number;
     hoursOffSolar: number;
     hours: number;
