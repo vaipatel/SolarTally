411115af9b382869e0e4be40b3b62f41129f1de8
Vai Patel <vai.pateln@gmail.com>
2019-12-03 19:06:06 -0500

    Add code to invoke UpdateApplianceUsage endpoint

--- src/SolarTally.WebUI_Ng/ClientApp/src/app/consumption/consumption.component.ts
+++ src/SolarTally.WebUI_Ng/ClientApp/src/app/consumption/consumption.component.ts
@@ -5,6 +5,9 @@ import { switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { ApplianceUsageLst, ApplianceUsage, ApplianceUsageLstStr, ApplianceUsageStr } from '../shared/dtos/appliance-usage';
 import { HttpErrorResponse } from '@angular/common/http';
+import { UpdateApplianceUsageCommandStr } from '../shared/dtos/update-appliance-usage-command';
+import { Consumption } from '../shared/dtos/consumption';
+import { UsageTimeInterval, UsageTimeIntervalStr } from '../shared/dtos/usage-time-interval';
 
 @Component({
   selector: 'app-consumption',
@@ -75,6 +78,58 @@ export class ConsumptionComponent implements OnInit {
 
   removeApplianceUsage(au: ApplianceUsageStr) {
     console.log("Will remove appliance usage " + au.id);
+  }
+
+  // updateApplianceUsage(au: ApplianceUsage) {
+  //   console.log("Will update appliance usage. Here is the command.");
+  //   let command = new UpdateApplianceUsageCommand();
+  //   command.consumptionId = +this.idParam.id;
+  //   command.applianceUsageId = au.id;
+  //   command.quantity = au.quantity;
+  //   command.powerConsumption = au.powerConsumption;
+  //   command.usageIntervals = new Array<UsageTimeIntervalAbrv>();
+  //   au.applianceUsageScheduleDto.usageIntervals.forEach((ui: UsageTimeInterval) => {
+  //     var newUTI = new UsageTimeIntervalAbrv();
+  //     newUTI.timeInterval = new TimeIntervalAbrv();
+  //     newUTI.timeInterval.start = new TimeSpanAbrv();
+  //     newUTI.timeInterval.start.hours = ui.timeInterval.start.hours;
+  //     newUTI.timeInterval.start.minutes = ui.timeInterval.start.minutes;
+  //     newUTI.timeInterval.end = new TimeSpanAbrv();
+  //     newUTI.timeInterval.end.hours = ui.timeInterval.end.hours;
+  //     newUTI.timeInterval.end.minutes = ui.timeInterval.end.minutes;
+  //     command.usageIntervals.push(newUTI);
+  //   })
+    
+  //   console.log(command);
+  //   this.api.updateApplianceUsage(command)
+  //   .subscribe((response: Consumption) => {
+  //     console.log("Response is:");
+  //     console.log(response);
+  //     // console.log(response.applianceUsages[0]);
+  //   });
+  // }
+
+  updateApplianceUsage(au: ApplianceUsageStr) {
+    console.log("Will update appliance usage. Here is the command.");
+    let command = new UpdateApplianceUsageCommandStr();
+    command.consumptionId = +this.idParam.id;
+    command.applianceUsageId = au.id;
+    command.quantity = au.quantity;
+    command.powerConsumption = au.powerConsumption;
+    command.usageIntervals = new Array<UsageTimeIntervalStr>();
+    au.applianceUsageScheduleDto.usageIntervals.forEach((ui: UsageTimeIntervalStr) => {
+      var newUTI = new UsageTimeIntervalStr();
+      newUTI.timeInterval = ui.timeInterval;
+      command.usageIntervals.push(newUTI);
+    });
+    
+    console.log(command);
+    this.api.updateApplianceUsage(command)
+    .subscribe((response: Consumption) => {
+      console.log("Response is:");
+      console.log(response);
+      // console.log(response.applianceUsages[0]);
+    });
   }
 
 }
